1500a23d42bf06adae568f903c3ec8f4d6aea2e5
Per Lindgren <per.lindgren@ltu.se>
2025-03-05 21:06:00 +0100

    100/1000 main

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // naive dft
 // https://en.wikipedia.org/wiki/Discrete_Fourier_transform
+
 #![allow(non_snake_case)]
 use num_complex::*;
 use std::f32::consts::TAU;
@@ -15,7 +16,6 @@ pub fn dft(x: &[f32]) -> Vec<Complex<f32>> {
         for (n, x) in x.iter().enumerate() {
             let nf = n as f32;
             let Xf = *x * c32(0.0, -nf * kf * TAU / Nf).exp();
-            println!("k {k}, n {n}, x {x}, Xf {Xf}");
             Xk += Xf;
         }
         X.push(Xk)
@@ -24,18 +24,16 @@ pub fn dft(x: &[f32]) -> Vec<Complex<f32>> {
 }
 
 // x being a real valued vector
-pub fn idft(X: &[Complex<f32>]) -> Vec<Complex<f32>> {
+pub fn idft_c(X: &[Complex<f32>]) -> Vec<Complex<f32>> {
     let N = X.len();
     let Nf = N as f32;
     let mut x = vec![];
     for k in 0..N {
-        println!("k {k}");
         let kf = k as f32;
         let mut xk = c32(0.0, 0.0);
         for (n, X) in X.iter().enumerate() {
             let nf = n as f32;
             let x = *X * c32(0.0, nf * kf * TAU / Nf).exp();
-            println!("k {k}, n {n}, x {X}, Xf {x}");
             xk += x;
         }
         x.push(xk / Nf);
@@ -43,18 +41,53 @@ pub fn idft(X: &[Complex<f32>]) -> Vec<Complex<f32>> {
     x
 }
 
+pub fn idft_r(X: &[Complex<f32>]) -> Vec<f32> {
+    let N = X.len();
+    let Nf = N as f32;
+    let mut x = vec![];
+    for k in 0..N {
+        let kf = k as f32;
+        let mut xk = 0.0;
+        for (n, X) in X.iter().enumerate() {
+            let nf = n as f32;
+            let x = ((*X) * c32(0.0, nf * kf * TAU / Nf).exp()).re();
+            xk += x;
+        }
+        x.push(xk / Nf);
+    }
+    x
+}
+
+pub fn gen_sin(fs: f32, t: f32, f: f32) -> Vec<f32> {
+    let mut x = vec![];
+    for i in 0..(fs * t) as usize {
+        x.push((f * TAU * i as f32 / fs).sin());
+    }
+    x
+}
+
 #[cfg(test)]
 mod test {
-    use std::process::id;
-
     use super::*;
 
     #[test]
-    fn test_dft() {
+    fn test_dft_c() {
         let X = dft(&[1.0, 0.0, 2.0]);
         println!("--- dft ---\n{X:?}");
 
-        let x = idft(X.as_ref());
+        let x = idft_c(X.as_ref());
         println!("--- idft ---\n{x:?}");
     }
+
+    #[test]
+    fn test_dft_r() {
+        let X = dft(&[1.0, 0.0, 2.0]);
+        println!("--- dft ---\n{X:?}");
+
+        let x = idft_r(X.as_ref());
+        println!("--- idft ---\n{x:?}");
+    }
+
+    #[test]
+    fn test_dft_sine() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use dft::*;
+
 fn main() {
-    println!("Hello, world!");
+    let x = gen_sin(1000.0, 1.0, 100.0);
+
+    let fft_x = dft(x.as_ref());
+    let ix = idft_r(fft_x.as_ref());
+    println!("{ix:?}");
 }
